f64e780ccadb2338ada55630daf0ca5edd068d8d
plaxswap <plaxswap@gmail.com>
2024-09-22 20:12:28 +0800

    update

--- packages/tokens/src/137.ts
+++ packages/tokens/src/137.ts
@@ -9,7 +9,7 @@ export const bscTokens = {
     '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
     18,
     'POL',
-    'Polygon Ecosystem Token',
+    'POL',
     'https://polygon.technology/',
   ),
   cake: CAKE_MAINNET,
