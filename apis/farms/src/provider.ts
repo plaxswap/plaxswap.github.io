7a41d2204e3d60de39d49ed4227fcc690a80ddde
plaxswap <plaxswap@gmail.com>
2025-05-01 00:01:53 +0800

    update

--- apis/farms/src/provider.ts
+++ apis/farms/src/provider.ts
@@ -2,7 +2,7 @@ import { StaticJsonRpcProvider } from '@ethersproject/providers'
 
 export const bscProvider = new StaticJsonRpcProvider(
   {
-    url: 'https://1rpc.io/37W31VzwTtHTisDEK/matic',
+    url: 'https://rpc.ankr.com/polygon/8a211590835087845ff22ee4159e7dc636f0cce7cfb1ecc483fc0fb1cbb99599',
     skipFetchSetup: true,
   },
   137,
--- scripts/updateLPsAPR.ts
+++ scripts/updateLPsAPR.ts
@@ -99,7 +99,7 @@ const getAprsForFarmGroup = async (addresses: string[], blockWeekAgo: number, ch
 // Copy paste of Stable farm logic
 export const bscProvider = new StaticJsonRpcProvider(
   {
-    url: 'https://1rpc.io/37W31VzwTtHTisDEK/matic',
+    url: 'https://rpc.ankr.com/polygon/8a211590835087845ff22ee4159e7dc636f0cce7cfb1ecc483fc0fb1cbb99599',
     skipFetchSetup: true,
   },
   137,
