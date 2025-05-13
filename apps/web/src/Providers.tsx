caaf1b1ceb2406d7e7f594c0b1d8e932064dfeff
plaxswap <plaxswap@gmail.com>
2025-05-14 00:14:12 +0800

    update

--- apps/web/src/Providers.tsx
+++ apps/web/src/Providers.tsx
@@ -10,7 +10,7 @@ import { client } from 'utils/wagmi'
 import { HistoryManagerProvider } from 'contexts/HistoryContext'
 import { createAppKit } from '@reown/appkit/react'
 import { polygon } from '@reown/appkit/networks'
-import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { Ethers5Adapter } from "@reown/appkit-adapter-ethers5";
 
 const projectId = '9ba1c138ff7ad815f7026b920b652f0b'
 const networks = [polygon]
@@ -23,7 +23,7 @@ const metadata = {
 }
 
 const modal = createAppKit({
-  adapters: [new EthersAdapter()],
+  adapters: [new Ethers5Adapter()],
   projectId: projectId,
   networks: [polygon],
   enableWalletGuide: false,
