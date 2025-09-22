c885fb6c7d82670797c4e1a8f7f275b266f6fc50
plaxswap <plaxswap@gmail.com>
2025-09-23 06:03:11 +0800

    add node

--- apis/farms/src/provider.ts
+++ apis/farms/src/provider.ts
@@ -2,7 +2,8 @@ import { StaticJsonRpcProvider } from '@ethersproject/providers'
 
 export const bscProvider = new StaticJsonRpcProvider(
   {
-    url: 'https://rpc.ankr.com/polygon/8a211590835087845ff22ee4159e7dc636f0cce7cfb1ecc483fc0fb1cbb99599',
+    // url: 'https://rpc.ankr.com/polygon/8a211590835087845ff22ee4159e7dc636f0cce7cfb1ecc483fc0fb1cbb99599',
+    url: 'https://1rpc.io/37W31VzwTtHTisDEK/matic',
     skipFetchSetup: true,
   },
   137,
--- apps/web/src/config/constants/rpc.ts
+++ apps/web/src/config/constants/rpc.ts
@@ -2,6 +2,7 @@ export const BSC_RPC_URLS = [
   'https://polygon-rpc.com',
   'https://polygon.llamarpc.com',
   'https://rpc.ankr.com/polygon',
+  'https://1rpc.io/matic',
 ]
 
 export const BSC_TESTNET_RPC_URLS = [
