54eeb4419b795a72b3d28eeaaf95c6751d63dbad
plaxswap <plaxswap@gmail.com>
2026-04-30 08:15:07 +0800

    remove headers origin

--- apps/web/src/utils/graphql.ts
+++ apps/web/src/utils/graphql.ts
@@ -5,24 +5,15 @@ import { INFO_CLIENT_WITH_CHAIN } from '../config/constants/endpoints'
 // Extra headers
 // Mostly for dev environment
 // No production env check since production preview might also need them
-export const getGQLHeaders = (endpoint: string) => {
-  if (endpoint === INFO_CLIENT) {
-    return {
-      origin:
-        process.env.NEXT_PUBLIC_NODE_REAL_HEADER ||
-        // hack for inject CI secret on window
-        (typeof window !== 'undefined' &&
-          // @ts-ignore
-          window.nrHeader),
-    }
-  }
+export const getGQLHeaders = () => {
+  
   return undefined
 }
 
 export const infoClient = new GraphQLClient(INFO_CLIENT)
 
 export const infoClientWithChain = (chainId: number) => {
-  return new GraphQLClient(INFO_CLIENT_WITH_CHAIN[chainId], { headers: getGQLHeaders(INFO_CLIENT_WITH_CHAIN[chainId]) })
+  return new GraphQLClient(INFO_CLIENT_WITH_CHAIN[chainId])
 }
 
 export const infoClientETH = new GraphQLClient(INFO_CLIENT_ETH)
