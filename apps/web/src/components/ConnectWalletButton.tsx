0c14e61dd2c16266ef6dd930e0e727aa60cb65d4
plaxswap <plaxswap@gmail.com>
2026-05-11 20:51:26 +0800

    wallet connect button custom

--- apps/web/src/components/ConnectWalletButton.tsx
+++ apps/web/src/components/ConnectWalletButton.tsx
@@ -1,14 +1,32 @@
 import { Button, ButtonProps } from '@pancakeswap/uikit'
 import { useAppKit } from '@reown/appkit/react'
 import { createElement } from 'react'
+import styled from 'styled-components'
 // @ts-ignore
 // eslint-disable-next-line import/extensions
 import { useActiveHandle } from 'hooks/useEagerConnect.bmp.ts'
 import Trans from './Trans'
 
-const ConnectWalletButton = ({ children, ...props }: ButtonProps) => {
+const StyledConnectWalletButton = styled(Button)`
+  background: linear-gradient(90deg, #42d7ee 0%, #8450d8 100%);
+  box-shadow: none;
+  color: #ffffff;
+
+  &:hover:not(:disabled):not(.pancake-button--disabled):not(:active) {
+    opacity: 0.85;
+  }
+`
+
+const ConnectWalletButton = ({ children, style, ...props }: ButtonProps) => {
   const handleActive = useActiveHandle()
   const { open } = useAppKit()
+  const restStyle = style ? { ...style } : undefined
+
+  if (restStyle) {
+    delete restStyle.background
+    delete restStyle.backgroundColor
+    delete restStyle.color
+  }
 
   const handleClick = () => {
     if (typeof __NEZHA_BRIDGE__ !== 'undefined') {
@@ -19,9 +37,9 @@ const ConnectWalletButton = ({ children, ...props }: ButtonProps) => {
   }
 
   return (
-    <Button onClick={handleClick} {...props}>
+    <StyledConnectWalletButton onClick={handleClick} style={restStyle} {...props}>
       {children || createElement(Trans, null, 'Connect Wallet')}
-    </Button>
+    </StyledConnectWalletButton>
   )
 }
 
